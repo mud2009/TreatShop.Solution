609c533b7c8ce7d79377489abefc39df4d04c39a
Will Greenberg <greenberg.will@gmail.com>
2022-03-25 16:07:40 -0700

    edit indentation, add register methods to account controller

--- TreatShop/Controllers/AccountController.cs
+++ TreatShop/Controllers/AccountController.cs
@@ -6,22 +6,41 @@ using TreatShop.ViewModels;
 
 namespace TreatShop.Controllers
 {
-    public class AccountController : Controller
+  public class AccountController : Controller
+  {
+    private readonly TreatShopContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, TreatShopContext db)
     {
-        private readonly TreatShopContext _db;
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly SignInManager<ApplicationUser> _signInManager;
-
-        public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, TreatShopContext db)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _db = db;
-        }
-
-        public ActionResult Index()
-        {
-            return View();
-        }
+      _userManager = userManager;
+      _signInManager = signInManager;
+      _db = db;
     }
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+    public IActionResult Register()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Register (RegisterViewModel model)
+    {
+      var user = new ApplicationUser { UserName = model.Email };
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
+  }
 }
