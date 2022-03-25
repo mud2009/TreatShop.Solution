851b056eeb287b01b6b3174e5ef8b3459959615b
Will Greenberg <greenberg.will@gmail.com>
2022-03-25 10:11:26 -0700

    add migrations, update database

---
+++ TreatShop/Migrations/20220325155907_ChangeFlavorName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TreatShop.Migrations
+{
+    public partial class ChangeFlavorName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Flavors",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Flavors",
+                newName: "Description");
+        }
+    }
+}
