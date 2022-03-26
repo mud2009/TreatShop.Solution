cd3067847a0315270b8b651813b22a9da8bc6823
Will Greenberg <greenberg.will@gmail.com>
2022-03-25 18:27:24 -0700

    remove password settings from startup

--- TreatShop/Startup.cs
+++ TreatShop/Startup.cs
@@ -35,12 +35,7 @@ namespace TreatShop
                 
       services.Configure<IdentityOptions>(options =>
       {
-          options.Password.RequireDigit = false;
-          options.Password.RequiredLength = 0;
-          options.Password.RequireLowercase = false;
-          options.Password.RequireNonAlphanumeric = false;
-          options.Password.RequireUppercase = false;
-          options.Password.RequiredUniqueChars = 0;
+        
       });
 
     }
